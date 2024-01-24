ecfebb7f683acc871cf615aae8e064f517217ce0
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-01-24 15:02:39 +0100

    Building the sidebar

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-
+import { Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 const DashboardLayout = () => {
+  const theme = useTheme();
 
+  const [selected, setSelected] = useState(0);
+  console.log(theme);
   return (
     <>
-      Dashboard Layout
+      <Box
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          height: "100vh",
+        }}
+      >
+        <Box sx={{ backgroundColor: theme.palette.background.paper }}></Box>
+      </Box>
       <Outlet />
     </>
   );
