0d264f32bf8ce6313219c8ece54568a6701d2ef9
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-01-25 14:12:04 +0100

    Added the switch logic to change the theme from light to dark mode

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,14 +1,59 @@
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Avatar, Box, Divider, IconButton, Stack, Switch } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
+import { styled, useTheme } from "@mui/material/styles";
 import Logo from "../../assets/Images/logo.ico";
 import { Nav_Buttons } from "../../data";
 import { Gear } from "phosphor-react";
 import { faker } from "@faker-js/faker";
+import useSettings from "../../hooks/useSettings";
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 40,
+  height: 20,
+  padding: 0,
+  display: "flex",
+  "&:active": {
+    "& .MuiSwitch-thumb": {
+      width: 15,
+    },
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      transform: "translateX(9px)",
+    },
+  },
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    "&.Mui-checked": {
+      transform: "translateX(20px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    transition: theme.transitions.create(["width"], {
+      duration: 200,
+    }),
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 20 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,.35)"
+        : "rgba(0,0,0,.25)",
+    boxSizing: "border-box",
+  },
+}));
+
 const DashboardLayout = () => {
   const theme = useTheme();
-
+  const { onToggleMode } = useSettings();
   const [selected, setSelected] = useState(0);
   console.log(theme);
   return (
@@ -102,7 +147,12 @@ const DashboardLayout = () => {
           </Stack>
 
           <Stack spacing={4}>
-            <Switch defaultChecked />
+            <AntSwitch
+              onChange={() => {
+                onToggleMode();
+              }}
+              defaultChecked
+            />
             <Avatar src={faker.image.avatar()} />
           </Stack>
         </Stack>
