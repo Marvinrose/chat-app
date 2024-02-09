8788a280d2add81a64f9a6180a033d20099a307b
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-02-09 15:07:44 +0100

    Updated simplebar-react and added pinned chats and all chats

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -10,9 +10,11 @@ import {
   Typography,
 } from "@mui/material";
 import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
-import { styled, alpha } from "@mui/material/styles";
+import { styled, alpha, useTheme } from "@mui/material/styles";
 import React from "react";
 import { faker } from "@faker-js/faker";
+import { ChatList } from "../../data";
+import { SimpleBarStyle } from "../../components/Scrollbar";
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {
@@ -43,36 +45,45 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
   },
 }));
 
-const ChatElement = () => {
+const ChatElement = ({ id, img, name, msg, time, unread, pinned, online }) => {
+  const theme = useTheme();
   return (
     <Box
       sx={{
         width: "100%",
         borderRadius: 1,
-        backgroundColor: "#fff",
+        backgroundColor:
+          theme.palette.mode === "light"
+            ? "#fff"
+            : theme.palette.background.default,
       }}
       p={2}
     >
       <Stack direction="row" alignItems="center" justifyContent="space-between">
         <Stack direction={"row"} spacing={2}>
-          <StyledBadge
-            overlap="circular"
-            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-            variant="dot"
-          >
-            {" "}
+          {online ? (
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
+            >
+              {" "}
+              <Avatar src={faker.image.avatar()} />
+            </StyledBadge>
+          ) : (
             <Avatar src={faker.image.avatar()} />
-          </StyledBadge>
+          )}
+
           <Stack spacing={0.3}>
-            <Typography variant="subtitle2">Pink Panda</Typography>
-            <Typography variant="caption">How are you?</Typography>
+            <Typography variant="subtitle2">{name}</Typography>
+            <Typography variant="caption">{msg}</Typography>
           </Stack>
         </Stack>
         <Stack spacing={2} alignItems="center">
           <Typography sx={{ fontWeight: 600 }} variant="caption">
-            9:36
+            {time}
           </Typography>
-          <Badge color="primary" badgeContent={2}></Badge>
+          <Badge color="primary" badgeContent={unread}></Badge>
         </Stack>
       </Stack>
     </Box>
@@ -82,7 +93,7 @@ const ChatElement = () => {
 const Search = styled("div")(({ theme }) => ({
   position: " relative",
   borderRadius: 20,
-  backgroundColor: alpha(theme.palette.background.paper, 1),
+  backgroundColor: alpha(theme.palette.background.default, 1),
   marginRight: theme.spacing(2),
   marginLeft: 0,
   width: "100%",
@@ -108,18 +119,21 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 const Chats = () => {
+  const theme = useTheme();
   return (
     <div>
       <Box
         sx={{
           position: "relative",
-          height: "100vh",
           width: 320,
-          backgroundColor: "#F8FAFF",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#F8FAFF"
+              : theme.palette.background.paper,
           boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
         }}
       >
-        <Stack p={3} spacing={2}>
+        <Stack p={3} spacing={2} sx={{ height: "100vh" }}>
           <Stack
             direction="row"
             alignItems="center"
@@ -148,14 +162,29 @@ const Chats = () => {
             </Stack>
             <Divider />
           </Stack>
-          <Stack direction={"column"}>
-            <Stack spacing={2.4}>
-              <Typography variant="subtitle2" sx={{ color: "#676767" }}>
-                Pinned
-              </Typography>
-              {}
-              <ChatElement />
-            </Stack>
+          <Stack
+            spacing={2}
+            direction={"column"}
+            sx={{ flexGrow: 1, overflow: "scroll", height: "100%" }}
+          >
+            <SimpleBarStyle timeout={500} clickOnTrack={false}>
+              <Stack spacing={2.4}>
+                <Typography variant="subtitle2" sx={{ color: "#676767" }}>
+                  Pinned
+                </Typography>
+                {ChatList.filter((el) => el.pinned).map((el) => {
+                  return <ChatElement {...el} />;
+                })}
+              </Stack>
+              <Stack spacing={2.4}>
+                <Typography variant="subtitle2" sx={{ color: "#676767" }}>
+                  All Chats
+                </Typography>
+                {ChatList.filter((el) => !el.pinned).map((el) => {
+                  return <ChatElement {...el} />;
+                })}
+              </Stack>
+            </SimpleBarStyle>
           </Stack>
         </Stack>
       </Box>
