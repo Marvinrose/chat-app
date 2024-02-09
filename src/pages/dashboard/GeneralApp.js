7b7e97e93332fce39a3c583ee77e9b850e372617
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-02-09 16:58:44 +0100

    Preparing the conversation component

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Chats from "./Chats";
+import { Box, Stack } from "@mui/material";
 
 const GeneralApp = () => {
   return (
     <>
-      {/* Chats */}
-      <Chats />
+      <Stack direction={"row"}>
+        {/* chats */}
+        <Chats />
+
+        <Box
+          sx={{
+            backgroundColor: "#fff",
+            height: "100%",
+            width: "calc(100vw - 420px)",
+          }}
+        >
+          {/* conversation */}
+        </Box>
+      </Stack>
     </>
   );
 };
