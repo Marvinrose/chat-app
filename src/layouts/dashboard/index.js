a2690c903be099765469db9e6db8ba36a4e9de17
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-02-14 16:35:32 +0100

    Created SideBar.js and added its contents

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -7,6 +7,7 @@ import { Gear } from "phosphor-react";
 import { faker } from "@faker-js/faker";
 import { Outlet } from "react-router-dom";
 import useSettings from "../../hooks/useSettings";
+import SideBar from "./SideBar";
 
 const AntSwitch = styled(Switch)(({ theme }) => ({
   width: 40,
@@ -60,116 +61,8 @@ const DashboardLayout = () => {
   return (
     <>
       <Stack direction="row">
-        <Box
-          p={2}
-          sx={{
-            backgroundColor: theme.palette.background.paper,
-            boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
-            height: "100vh",
-            width: 100,
-          }}
-        >
-          <Stack
-            direction="column"
-            alignItems="center"
-            justifyContent="space-between"
-            sx={{ height: "100%" }}
-            spacing={3}
-          >
-            <Stack alignItems="center" spacing={4}>
-              <Box
-                sx={{
-                  backgroundColor: theme.palette.primary.main,
-                  height: 64,
-                  width: 64,
-                  borderRadius: 1.5,
-                }}
-              >
-                <img alt="logo" src={Logo}></img>
-              </Box>
-              <Stack
-                spacing={3}
-                sx={{ width: "max-content" }}
-                direction="column"
-                alignItems="center"
-              >
-                {Nav_Buttons.map((el) =>
-                  el.index === selected ? (
-                    <Box
-                      p={1}
-                      sx={{
-                        backgroundColor: theme.palette.primary.main,
-                        borderRadius: 1.5,
-                      }}
-                    >
-                      {" "}
-                      <IconButton
-                        key={el.index}
-                        sx={{ width: "max-content", color: "#fff" }}
-                      >
-                        {el.icon}
-                      </IconButton>
-                    </Box>
-                  ) : (
-                    <IconButton
-                      onClick={() => {
-                        setSelected(el.index);
-                      }}
-                      key={el.index}
-                      sx={{
-                        width: "max-content",
-                        color:
-                          theme.palette.primary.mode === "light"
-                            ? "#000"
-                            : theme.palette.text.primary,
-                      }}
-                    >
-                      {el.icon}
-                    </IconButton>
-                  )
-                )}
-                <Divider sx={{ width: "48px" }} />
-                {selected === 3 ? (
-                  <Box
-                    p={1}
-                    sx={{
-                      backgroundColor: theme.palette.primary.main,
-                      borderRadius: 1.5,
-                    }}
-                  >
-                    <IconButton sx={{ color: "#fff" }}>
-                      <Gear></Gear>
-                    </IconButton>
-                  </Box>
-                ) : (
-                  <IconButton
-                    onClick={() => {
-                      setSelected(3);
-                    }}
-                    sx={{
-                      color:
-                        theme.palette.primary.mode === "light"
-                          ? "#000"
-                          : theme.palette.text.primary,
-                    }}
-                  >
-                    <Gear></Gear>
-                  </IconButton>
-                )}
-              </Stack>
-            </Stack>
-
-            <Stack spacing={4}>
-              <AntSwitch
-                onChange={() => {
-                  onToggleMode();
-                }}
-                defaultChecked
-              />
-              <Avatar src={faker.image.avatar()} />
-            </Stack>
-          </Stack>
-        </Box>
+        {/* SideBar */}
+        <SideBar />
         <Outlet />
       </Stack>
     </>
