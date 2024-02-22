53c461532b17df57ca3e331752ad86ed0ab52674
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2024-02-22 14:09:27 +0100

    Built settings sidebar

--- src/pages/dashboard/Settings.js
+++ src/pages/dashboard/Settings.js
@@ -1,9 +1,80 @@
 import React from "react";
-import { Box, Stack } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import {
+  Bell,
+  CaretLeft,
+  Image,
+  Info,
+  Key,
+  Keyboard,
+  Lock,
+  Note,
+  PencilCircle,
+} from "phosphor-react";
+import { faker } from "@faker-js/faker";
 
 const Settings = () => {
   const theme = useTheme();
+
+  const List = [
+    {
+      key: 0,
+      icon: <Bell size={20} />,
+      title: "Notifications",
+      onclick: () => {},
+    },
+    {
+      key: 1,
+      icon: <Lock size={20} />,
+      title: "Privacy",
+      onclick: () => {},
+    },
+    {
+      key: 2,
+      icon: <Key size={20} />,
+      title: "Security",
+      onclick: () => {},
+    },
+    {
+      key: 3,
+      icon: <PencilCircle size={20} />,
+      title: "Theme",
+      onclick: () => {},
+    },
+    {
+      key: 4,
+      icon: <Image size={20} />,
+      title: "Chat Wallpaper",
+      onclick: () => {},
+    },
+    {
+      key: 5,
+      icon: <Note size={20} />,
+      title: "Request Account Info",
+      onclick: () => {},
+    },
+    {
+      key: 6,
+      icon: <Keyboard size={20} />,
+      title: "Keyboard Shortcuts",
+      onclick: () => {},
+    },
+    {
+      key: 7,
+      icon: <Info size={20} />,
+      title: "Help",
+      onclick: () => {},
+    },
+  ];
+
   return (
     <>
       <Stack direction="row" sx={{ width: "100%" }}>
@@ -22,8 +93,52 @@ const Settings = () => {
         >
           <Stack p={4} spacing={5}>
             {/* header */}
+            <Stack
+              sx={{ height: "100%", p: 2 }}
+              direction="row"
+              alignItems="center"
+              spacing={3}
+            >
+              <IconButton>
+                <CaretLeft size={24} color={"#4B4B4B"} />
+              </IconButton>
+              <Typography variant="h6">Settings</Typography>
+            </Stack>
+
             {/* profile */}
+            <Stack direction={"row"} spacing={3}>
+              <Avatar
+                width={56}
+                height={56}
+                src={faker.image.avatar()}
+                alt={faker.name.firstName()}
+              />
+              <Stack direction="column" spacing={0.5}>
+                <Typography variant="article">
+                  {faker.name.fullName()}
+                </Typography>
+                <Typography variant="body2">{faker.random.words()}</Typography>
+              </Stack>
+            </Stack>
+
             {/* list of options */}
+            <Stack spacing={4}>
+              {List.map(({ key, icon, title, onclick }) => (
+                <>
+                  <Stack
+                    onClick={onclick}
+                    sx={{ cursor: "pointer" }}
+                    spacing={2}
+                  >
+                    <Stack direction="row" spacing={2} alignItems="center">
+                      {icon}
+                      <Typography variant="body2">{title}</Typography>
+                    </Stack>
+                    {key !== 7 && <Divider />}
+                  </Stack>
+                </>
+              ))}
+            </Stack>
           </Stack>
         </Box>
 
